65381130cc3247778dd462254155c0c285440c59
Priyanshupatel1 <Patelpriyanshu321@gmail.com>
2023-02-11 15:26:57 +0530

    created a new server with GET and POST

---
+++ README.md
@@ -0,0 +1,15 @@
+# first_server
+1) Json file start ka value rakh die toh npm start krne pe le lega.
+cases when server nameis big we use this method.
+
+2) start used here is defined keyword .. but we can use self declared name to run we just need to use run before declared variable;
+"runthis" : "node server.js" => in this we can run the serve by using (npm run runthis).
+
+3) nodemon is a type of server runner = > using this we dont need to run server again again.
+
+4) nodemon ki sari dependencies are in package-lock .
+
+5)  .gitignore => used to ignore files u dont wanna upload on git hub;
+ 
+6) and then if someone want to download those files under gitignore user can use => npm i in terminal;
+
---
+++ server.js
@@ -0,0 +1,72 @@
+//server creation
+//basic server very minimal
+const http = require("http"); //require keyworrd useed to import all the mnodules that are being used
+const toDoList = ["Need to learn", "Need to code"];
+
+//http method
+
+// GET =>GETTING certain details from server pass this request on browser
+// PUT = > ovewrite ,fully update
+// DELETE => deleting  data from server
+// PATCH => update very few fields / certain fields;
+// POST => sending to the server;
+
+
+
+
+
+const port = 8081;// this is free port make sure that port is free and not engaged
+
+http.createServer((req, res) => {// user can use anything 
+    // res.writeHead(200,{"Content-Type": "text/html"});// 200 is the statius/indication code of succesfully made server 
+    // //respose me content ka kya type hai voh lega ("Content-Type": "text/html")
+
+    // res.write("<h4>hello Priyanshu, this is new server</h4>");//when user use key within object then dot operator
+    // res.end();//end response
+
+    const { method, url } = req;
+    // console.log(method , url);
+    // res.end();
+    if (url === "/todos") {
+        // http://localhost:8081/todos
+
+        if (method === "GET") {//is condtion pe run krega
+            res.writeHead(200, { "Content-Type": "text/html" }); // when content type not given then then black background; 
+            res.write(toDoList.toString());//string value kr dega and show krega
+            
+        } 
+        else if (method === "POST") {
+            let body = "";//variable
+            req.on('error', (err) => {//on means whenever it get triggered;
+                console.log(err);
+            }).on('data', (chunk) => 
+            {   // body = body + chunk;
+                body += chunk;//body me append krega
+               console.log("chunk of data : ",chunk);//chunk is the data which is sent in body
+            }).on('end',() =>{
+                body = JSON.parse(body);//isko human understable form me convert kr rhe hai //string conversion;
+                console.log("body data" , body);
+            });
+         // res.writeHead(501);
+        }
+
+        else {//error message show krne ke lie;
+            res.writeHead(501);//server response should be with writehead
+            }
+    } 
+        else {
+        res.writeHead(404);
+    }
+    res.end();
+}).listen(port, () => {//continuously listen to all the changes user makes
+    console.log(`my Nodejs sever started on port ${port}`);
+});
+
+//routing from one file to other
+// http://localhost:8081
+// http://localhost:8081/
+// http://localhost:8081/home
+// http://localhost:8081/aboutUs    =-> navigation
+// http://localhost:8081/contactUs
+
+//cntrl + c se server end kr dega

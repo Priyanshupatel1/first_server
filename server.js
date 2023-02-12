b1a41a2c80b7d1656c0f1663d0aae13203687aa6
Priyanshupatel1 <Patelpriyanshu321@gmail.com>
2023-02-12 22:31:48 +0530

    server-created-using-express

--- server.js
+++ server.js
@@ -1,72 +1,50 @@
-//server creation
-//basic server very minimal
-const http = require("http"); //require keyworrd useed to import all the mnodules that are being used
+const express = require('express');
+
+const app = express();//app ko access krke express ke sare modules use kar skte . 
+
+//appln will now use json format for the data transfer
+app.use(express.json());
+//const hostname = "priyanshu";
+const port = 8081;
+
 const toDoList = ["Need to learn", "Need to code"];
 
-//http method
+//http://localhost:8081/todos 
 
-// GET =>GETTING certain details from server pass this request on browser
-// PUT = > ovewrite ,fully update
-// DELETE => deleting  data from server
-// PATCH => update very few fields / certain fields;
-// POST => sending to the server;
+app.get("/todos" , (req,res) =>{
+    res.status(200).send(toDoList);
+    });
 
+    app.post("/todos",(req,res)=> {
+ let newToDoItem = req.body.item;
+ toDoList.push(newToDoItem);
+ res.status(201).send({
+    message  : "the to do get added got succesfuly"
+ });
+    });
+app.delete("/todos",(req,res) =>{
+    let itemToDelete = req.body.item;
 
-
-
-
-const port = 8081;// this is free port make sure that port is free and not engaged
-
-http.createServer((req, res) => {// user can use anything 
-    // res.writeHead(200,{"Content-Type": "text/html"});// 200 is the statius/indication code of succesfully made server 
-    // //respose me content ka kya type hai voh lega ("Content-Type": "text/html")
-
-    // res.write("<h4>hello Priyanshu, this is new server</h4>");//when user use key within object then dot operator
-    // res.end();//end response
-
-    const { method, url } = req;
-    // console.log(method , url);
-    // res.end();
-    if (url === "/todos") {
-        // http://localhost:8081/todos
-
-        if (method === "GET") {//is condtion pe run krega
-            res.writeHead(200, { "Content-Type": "text/html" }); // when content type not given then then black background; 
-            res.write(toDoList.toString());//string value kr dega and show krega
-            
-        } 
-        else if (method === "POST") {
-            let body = "";//variable
-            req.on('error', (err) => {//on means whenever it get triggered;
-                console.log(err);
-            }).on('data', (chunk) => 
-            {   // body = body + chunk;
-                body += chunk;//body me append krega
-               console.log("chunk of data : ",chunk);//chunk is the data which is sent in body
-            }).on('end',() =>{
-                body = JSON.parse(body);//isko human understable form me convert kr rhe hai //string conversion;
-                console.log("body data" , body);
-            });
-         // res.writeHead(501);
+    toDoList.find((element,index)=>{
+        if(element === itemToDelete){
+            toDoList.splice(index,1);
         }
-
-        else {//error message show krne ke lie;
-            res.writeHead(501);//server response should be with writehead
-            }
-    } 
-        else {
-        res.writeHead(404);
-    }
-    res.end();
-}).listen(port, () => {//continuously listen to all the changes user makes
-    console.log(`my Nodejs sever started on port ${port}`);
+    });
+    res.status(201).send({message : `deleted item ${req.body.item}`,});//204 is for no content is does not shows any content
+});
+//one route which will handle all the routes which are not explicitly mentioned
+app.all("/todos",(req,res)=>{
+    res.status(501).send({
+        message : "THIS METHOD IS NOT IMPLEMENTED"
+    });
 });
 
-//routing from one file to other
-// http://localhost:8081
-// http://localhost:8081/
-// http://localhost:8081/home
-// http://localhost:8081/aboutUs    =-> navigation
-// http://localhost:8081/contactUs
+app.all("*",(req,res)=>{//" * " this here repesents everything other than "/todos"
+    res.status(404).send();//this error code holds priority here id stated at the top it wont allow anything but state only error message
+});
 
-//cntrl + c se server end kr dega
+
+
+    app.listen(port ,()=>{
+        console.log(`node js server started at ${port}`)
+    });
